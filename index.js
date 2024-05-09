33cfd873f39a031f6863b90def38c774aee4acfb
CodeChef <joetomsim@gmail.com>
2024-05-08 23:03:39 -0500

    added role and dept functions

--- index.js
+++ index.js
@@ -52,18 +52,8 @@ function init() {
           pool.end();
           break;
       }
-    });
-}
-
-//example pool query for reference
-// pool.query('SELECT NOW()', (err, res) => {
-//   if (err) {
-//       console.error('Error executing query', err);
-//   } else {
-//       console.log('Connected to PostgreSQL database:', res.rows[0].now);
-//   }
-//   pool.end(); // Close the connection pool
-// });
+    })
+};
 
 //function to view all employees
 //All emp> emp ids, first name, last name, job title, depts, salaries, and managers reported to
@@ -78,41 +68,43 @@ function viewAllEmployees() {
     if (err) throw err;
     console.table(res.rows);
     init();
-  });
+  })
 };
 
 //function to add an employee
 //Add emp> prompted to enter first name, last name, role, manager, and is added to db
-function addEmployee() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "fname",
-        message: "Please enter employee's first name:"
-      },
-      {
-        type: "input",
-        name: "lname",
-        message: "Please enter employee's last name:"
-      },
-      {
-        type: "list",
-        name: "role",
-        choices: 
-      },
-      {
-        type: "list",
-        name: "manager",
-        choices: 
-      },
-    ]) .then((input) => )
-  }
+// function addEmployee() {
+//   inquirer
+//     .prompt([
+//       {
+//         type: "input",
+//         name: "fname",
+//         message: "Please enter employee's first name:"
+//       },
+//       {
+//         type: "input",
+//         name: "lname",
+//         message: "Please enter employee's last name:"
+//       },
+//       {
+//         type: "list",
+//         name: "role",
+//         choices: 
+//       },
+//       {
+//         type: "list",
+//         name: "manager",
+//         choices: 
+//       },
+//     ]).then((input) => )
+// }
 
 
 //function to update an employee roles
 //Update emp> prompted to select emp and update their new role, and is updated on db
-// function updateEmployeeRole();
+function updateEmployeeRole() {
+
+}
 
 
 //function to view all roles
@@ -126,14 +118,44 @@ function viewAllRoles() {
     if (err) throw err;
     console.table(res.rows);
     init();
-  });
-}
-
+  })
+};
 
 //function to add a role
-//Add role> prompted to enter name, salary, dept, and is added to db
-// function addRole();
-
+// Add role> prompted to enter name, salary, dept, and is added to db
+function addRole() {
+  const query = `SELECT * FROM dept`;
+  pool.query(query, (err, res) => {
+    if (err) throw err;
+      const departments = res.rows.map(({id, name}) => ({name:name, value:id}))
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Please enter the name for the new role:"
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "Please enter the salary for this role:"
+        },
+        {
+          type: "list",
+          name: "dept",
+          choices: departments
+        }
+      ]).then((input) => {
+        const query = `INSERT INTO role(title, salary, dept_id) VALUES ($1, $2, $3)`;
+        pool.query(query, [input.name, input.salary, input.dept], (err, res) => {
+          if (err) throw err;
+          console.log(`New role ${input.name} successfully added!`);
+          init();
+        })
+      })
+  }
+  )
+};
 
 //function to view all departments
 //All depts> table showing dept names and id
@@ -144,20 +166,27 @@ function viewAllDepartments() {
     if (err) throw err;
     console.table(res.rows);
     init();
-  });
-}
-
+  })
+};
 
 //function to add a department
 //Add dept> prompted to enter dept name, is added to db
-// function addDepartment();
-
-
-
-
-
-
-
-
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the name of the new department:"
+      }
+    ]).then((input) => {
+      const query = `INSERT INTO dept (name) VALUES ($1)`;
+      pool.query(query, [input.name], (err, res) => {
+        if (err) throw err;
+        console.log(`New department ${input.name} successfully added!`);
+        init();
+      })
+    })
+};
 
 init();
